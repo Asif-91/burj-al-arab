2b5414d5a976ab74bce945d72f5b761e28576f92
Asif-91 <asifchowdhury323@gmail.com>
2020-09-14 16:15:53 +0600

    redirect is done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA4mSDvtEeS0HfJH50O1Ylwr25oWFvDyiI",
+    authDomain: "burj-al-arab-cf9d3.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-cf9d3.firebaseio.com",
+    projectId: "burj-al-arab-cf9d3",
+    storageBucket: "burj-al-arab-cf9d3.appspot.com",
+    messagingSenderId: "901993051894",
+    appId: "1:901993051894:web:f117f36c40ab08233c1ed7"
+  };
+
+
+  export default firebaseConfig;
